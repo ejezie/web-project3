20d5b1eee4e663d476c1ab3c93d4decb30625046
Ejezie <shady07chinex@gmail.com>
2020-08-29 09:59:42 +0100

    Selecting classes, creating variables

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // Declaring variables not war
 
 const cartBtn = document.querySelector('.cart-btn');
-const closetBtn = document.querySelector('.close-btn');
+const closetCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
+
+// cart items
+let cart = []
